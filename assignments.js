0076a216b49d2ed618ed86125a5819c3b7df4d47
vamshi-chinna <vamshi30082002@gmail.com>
2023-07-28 15:14:05 +0530

    changes

---
+++ assignments.js
@@ -0,0 +1,8 @@
+let a = prompt("CHEACK wheater the num is divisible by 7 or not")
+if(a%7===0){
+    console.log("it is divisable by 7")
+
+}
+else{
+    console.log("it is not divisable by 7")
+}
--- todo.js
+++ todo.js
@@ -2,27 +2,27 @@ const form = document.getElementById("form")
 let result = document.getElementById("text")
 const container = document.getElementById("container")
 
+const containerItem = document.createElement("div");
+const task = document.createElement("div");
+const check = document.createElement("input")
+const rmBtn = document.createElement("button")
 
-
-function addElementFunction() {
-    const e = document.createElement("div");
-    const task = document.createElement("div");
-    const check = document.createElement("input")
-    const rmBtn = document.createElement("button")
+async function addElementFunction() {
+   
     check.type = "checkbox"
     check.setAttribute("class", "check")
     task.setAttribute("class", "task")
-    rmBtn.setAttribute("class", "container-item-btn")
-    e.setAttribute("class", "container-item")
-    r = result.value;
-    e.style.textTransform = "capitalize"
+    rmBtn.setAttribute("class", "rmbtn")
+    containerItem.setAttribute("class", "container-item")
+ 
+    containerItem.style.textTransform = "capitalize"
     rmBtn.innerHTML = "X"
-    task.innerHTML = `${r}`
+    task.innerHTML = result.value;
   
-    e.append(check)
-    e.append(task);
-    e.append(rmBtn)
-    container.append(e)
+    containerItem.append(check)
+    containerItem.append(task);
+    containerItem.append(rmBtn)
+    container.append(containerItem)
 
 
     result.value = " "
@@ -31,39 +31,52 @@ function addElementFunction() {
         if(check.checked){
             task.style.textDecoration="line-through"
             console.log("done")
+            
         }
         else{
             task.style.textDecoration="none"
+            
         }
       
     }
     check.addEventListener("click",(e)=>{
         e.preventDefault
         checkbox();
+        
         console.log("done")
 
     })
-    function remove() {
-        const element = e
-        element.remove();
-    }
-    rmBtn.addEventListener("click", (e) => {
-        e.preventDefault();
-        remove()
-
-    })
-    let n=check.value
-    console.log(n)
-
-
+   
+    
+ 
+ 
+    
 }
+
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     addElementFunction()
+    store()
 
 })
 btn.addEventListener("click", (e) => {
     e.preventDefault();
     addElementFunction()
+    
 })
+
+
+function remove() {
+    const element = containerItem
+    element.remove();
+    
+    
+}rmBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    remove()
+    
+    
+})
+
+//    container.innerHTML=  localStorage.clear("data")
